175abc09a01c9b73e8aab4d644383e8bd9f01564
Gk emon <emon.info2013@gmail.com>
2020-01-26 21:22:37 +0600

    Add last goal in UI

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 // this.props.match.params.teamID
 class Details extends Component {
-    state = {  }
+    constructor(props) {
+        super(props);
+    this.state = {  
+      homeTeam : "",
+      awayTeam : "",
+      homeTeamGoal : "",
+      awayTeamGoal : ""
+    }
+}
     componentDidMount() {
         this.fetchLastMatchData();
     }
+
 
     fetchLastMatchData() {
         const Token = "b7d52e61c66f4a0194be725042ad4359",
@@ -17,7 +26,22 @@ class Details extends Component {
             .then(response => response.json())
             .then(responseJson => {
                 
-                console.log(responseJson.matches[Object.keys(responseJson.matches).length-1].score.winner);
+        
+                console.log(responseJson.matches[Object.keys(responseJson.matches).
+                    length-1].score.winner);
+
+
+                this.setState({
+                    homeTeamGoal:responseJson.matches[Object.keys(responseJson.matches).
+                        length - 1].score.fullTime.homeTeam ,
+                    homeTeam : responseJson.matches[Object.keys(responseJson.matches).
+                        length - 1].homeTeam.name ,
+                    awayTeamGoal: responseJson.matches[Object.keys(responseJson.matches).
+                        length - 1].score.fullTime.awayTeam,
+                     awayTeam: responseJson.matches[Object.keys(responseJson.matches).
+                        length - 1].awayTeam.name 
+                }) ; 
+            
                 
             })
             .catch(error => {
@@ -25,8 +49,10 @@ class Details extends Component {
             });
     }
     render() { 
-        return (<div class="centered">
-            <h1></h1>
+        return (<div>
+            <h1>Result of last match</h1>
+            <h3>{this.state.homeTeam} - {this.state.homeTeamGoal}:
+             {this.state.awayTeamGoal} - {this.state.awayTeam}  </h3>
             </div>);
     }
 }
